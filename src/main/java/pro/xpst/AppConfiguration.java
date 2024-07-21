8e854907260964f1cb5701645380f7718a7d89ac
xpst <7100711+xpst@users.noreply.github.com>
2024-07-21 22:42:25 +0200

    Workaround for SocketTimeoutException: https://github.com/spring-projects/spring-ai/issues/354

--- src/main/java/pro/xpst/AppConfiguration.java
+++ src/main/java/pro/xpst/AppConfiguration.java
@@ -1,11 +1,16 @@
 package pro.xpst;
 
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.longpolling.BotSession;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import pro.xpst.telegram.OpenAiTelegramBot;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfiguration {
@@ -18,5 +23,17 @@ public class AppConfiguration {
     @Bean
     public TelegramBotsLongPollingApplication application() {
         return new TelegramBotsLongPollingApplication();
+    }
+
+    @Bean
+    public RestClientCustomizer restClientCustomizer() {
+        return restClientBuilder -> restClientBuilder
+                .requestFactory(
+                        ClientHttpRequestFactories.get(
+                            ClientHttpRequestFactorySettings.DEFAULTS
+                                .withConnectTimeout(Duration.ofSeconds(60))
+                                .withReadTimeout(Duration.ofSeconds(60))
+                        )
+                );
     }
 }
